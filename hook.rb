cd1213c0ae28663babe5314f7f614bf4633cf811
Chris Kampmeier <chris@kampers.net>
2009-03-28 00:31:28 -0700

    More porting of the bash script to figure out the type of object that was pushed

--- hook.rb
+++ hook.rb
@@ -7,8 +7,8 @@ end
 
 
 refname = ARGV[0]
-oldrev  = `git rev-parse #{ARGV[1]}`
-newrev  = `git rev-parse #{ARGV[2]}`
+oldrev  = `git rev-parse #{ARGV[1]}`.strip
+newrev  = `git rev-parse #{ARGV[2]}`.strip
 
 change_type =
   if oldrev =~ /^0*$/
@@ -19,8 +19,8 @@ change_type =
     :update
   end
 
-newrev_type = `git cat-file -t #{newrev} 2> /dev/null`
-oldrev_type = `git cat-file -t #{oldrev} 2> /dev/null`
+newrev_type = `git cat-file -t #{newrev} 2> /dev/null`.strip
+oldrev_type = `git cat-file -t #{oldrev} 2> /dev/null`.strip
 
 case change_type
 when :create, :update
@@ -28,3 +28,18 @@ when :create, :update
 when :delete
   rev, rev_type = oldrev, oldrev_type
 end
+
+
+if refname =~ %r{^refs/(tags|heads|remotes)/(.+)$}
+  short_refname = $2
+
+  refname_types = {%w(tags    commit)  => "tag",
+                   %w(tags    tag)     => "annotated tag",
+                   %w(heads   commit)  => "branch",
+                   %w(remotes commit)  => "tracking branch"}
+  refname_type = refname_types[[$1, rev_type]]
+end
+
+
+puts refname_type
+puts short_refname
