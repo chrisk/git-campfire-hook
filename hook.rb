dc1ceaed58e8262f916f2a0b0dfc57baf34c6b89
Chris Kampmeier <chris@kampers.net>
2009-03-28 23:03:38 -0700

    Big refactor to be more Rubyish

--- hook.rb
+++ hook.rb
@@ -5,90 +5,110 @@ if ARGV.length != 3
   exit
 end
 
-
 refname = ARGV[0]
 oldrev  = `git rev-parse #{ARGV[1]}`.strip
 newrev  = `git rev-parse #{ARGV[2]}`.strip
 
-change_type =
-  if oldrev =~ /^0*$/
-    :create
-  elsif newrev =~ /^0*$/
-    :delete
-  else
-    :update
+
+
+
+class GitCampfireNotification
+
+  def initialize(refname, oldrev, newrev)
+    @ref_name     = refname
+    @old_revision = oldrev
+    @new_revision = newrev
+    @old_revision_type = `git cat-file -t #{oldrev} 2> /dev/null`.strip
+    @new_revision_type = `git cat-file -t #{newrev} 2> /dev/null`.strip
+
+    if ref_name_type.include?("branch")
+      send("create_branch", refname, oldrev, newrev)
+    end
   end
 
-newrev_type = `git cat-file -t #{newrev} 2> /dev/null`.strip
-oldrev_type = `git cat-file -t #{oldrev} 2> /dev/null`.strip
 
-case change_type
-when :create, :update
-  rev, rev_type = newrev, newrev_type
-when :delete
-  rev, rev_type = oldrev, oldrev_type
-end
-
-
-if refname =~ %r{^refs/(tags|heads|remotes)/(.+)$}
-  short_refname = $2
-
-  refname_types = {%w(tags    commit) => "tag",
-                   %w(tags    tag)    => "annotated tag",
-                   %w(heads   commit) => "branch",
-                   %w(remotes commit) => "tracking branch"}
-  refname_type = refname_types[[$1, rev_type]]
-end
-
-
-def update_branch(refname, oldrev, newrev)
-  if `git rev-list #{newrev}..#{oldrev}`.empty?
-    update_type = :fast_foward
-  elsif newrev == `git merge-base #{oldrev} #{newrev}`
-    update_type = :rewind
-    # TODO: say something about the rewind
-  else
-    update_type = :force
-    # TODO: say something about the force-push
+  def project_name
+    project_name = `git rev-parse --git-dir 2>/dev/null`.strip
+    if project_name == ".git"
+      project_name = File.basename(Dir.pwd)
+    end
+    project_name.sub(/\.git$/, "")
   end
 
-  unless update_type == :rewind
-    speak_new_commits(new_commits("update", refname, oldrev, newrev))
+  def change_type
+    if @old_revision =~ /^0*$/
+      :create
+    elsif @new_revision =~ /^0*$/
+      :delete
+    else
+      :update
+    end
   end
-end
 
-def create_branch(refname, oldrev, newrev)
-  # TODO: say something about the branch creation
-  speak_new_commits(new_commits("create", refname, oldrev, newrev))
-end
-
-def new_commits(change_type, refname, oldrev, newrev)
-  revision_range = (change_type == :create) ? newrev : "#{oldrev}..#{newrev}"
-
-  other_branches = `git for-each-ref --format='%(refname)' refs/heads/ | grep -F -v #{refname}`
-  sentinel = "=-=-*-*-" * 10
-  raw_commits = `git rev-parse --not #{other_branches} | git rev-list --reverse --pretty=format:'%cn%n%s%n%n%b#{sentinel}' --stdin #{revision_range}`.split(sentinel)
-  raw_commits.pop # last is empty because there's an ending sentinel
-
-  raw_commits.inject([]) { |commits, raw_commit|
-    lines = raw_commit.strip.split("\n")
-    commits << {:revision  => lines[0].sub(/^commit /, ""),
-                :committer => lines[1],
-                :message   => lines[2..-1].join("\n")}
-  }
-end
-
-
-def speak_new_commits(commits)
-  commits.each do |c|
-    # TODO: replace puts with tinder
-    puts "#{c[:committer]} just committed #{c[:revision]}"
-    puts c[:message]
-    puts
+  def short_ref_name
+    @ref_name.match(%r{^refs/(?:tags|heads|remotes)/(.+)$})[1]
   end
+
+  def ref_name_type
+    rev_type = (change_type == :delete) ? @old_revision_type : @new_revision_type
+    ref_name_types = {%w(tags    commit) => "tag",
+                      %w(tags    tag)    => "annotated tag",
+                      %w(heads   commit) => "branch",
+                      %w(remotes commit) => "tracking branch"}
+    @ref_name.match(%r{^refs/(tags|heads|remotes)/.+$})
+    ref_name_types[[$1, rev_type]]
+  end
+
+
+  def new_commits
+    revision_range = (change_type == :create) ? @new_revision : "#{@old_revision}..#{@new_revision}"
+
+    other_branches = `git for-each-ref --format='%(refname)' refs/heads/ | grep -F -v #{@ref_name}`
+    sentinel = "=-=-*-*-" * 10
+    raw_commits = `git rev-parse --not #{other_branches} | git rev-list --reverse --pretty=format:'%cn%n%s%n%n%b#{sentinel}' --stdin #{revision_range}`.split(sentinel)
+    raw_commits.pop # last is empty because there's an ending sentinel
+
+    raw_commits.inject([]) { |commits, raw_commit|
+      lines = raw_commit.strip.split("\n")
+      commits << {:revision  => lines[0].sub(/^commit /, ""),
+                  :committer => lines[1],
+                  :message   => lines[2..-1].join("\n")}
+    }
+  end
+
+  def speak_new_commits
+    new_commits.each do |c|
+      # TODO: replace puts with tinder
+      puts "#{c[:committer]} just committed #{c[:revision]}"
+      puts c[:message]
+      puts
+    end
+  end
+
+
+  def update_branch
+    if `git rev-list #{@new_revision}..#{@old_revision}`.empty?
+      update_type = :fast_foward
+    elsif newrev == `git merge-base #{@old_revision} #{@new_revision}`
+      update_type = :rewind
+      puts "Notce: the #{project_name}/#{short_ref_name} branch was just rewound"
+    else
+      update_type = :force
+      puts "Notice: the #{project_name}/#{short_ref_name} branch was just force-updated"
+    end
+
+    unless update_type == :rewind
+      speak_new_commits
+    end
+  end
+
+  def create_branch(refname, oldrev, newrev)
+    puts "A new branch was just pushed to #{project_name}/#{short_ref_name}:"
+    speak_new_commits
+  end
+
 end
 
 
-if refname_type.include?("branch")
-  send("#{change_type}_branch", refname, oldrev, newrev)
-end
+
+GitCampfireNotification.new(refname, oldrev, newrev)
